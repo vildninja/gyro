3277ba0e32febd75f6a432bd10c1dc6ceec0ce61
Jannek <jannek.ek@gmail.com>
2015-09-19 17:30:51 +0200

    init

---
+++ Assets/GyroPhone/PhoneController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace VildNinja.GyroPhone
+{
+    public class PhoneController : MonoBehaviour
+    {
+        public int number;
+
+        public Text text;
+
+        private float vibrate;
+
+        private int host;
+        private int connection;
+        private int state;
+        private int reliable;
+
+        private byte error;
+
+        private bool isConnected;
+
+        public readonly byte[] broadcast = new byte[1000];
+        public readonly byte[] data = new byte[1000];
+
+        private MemoryStream ms;
+        private BinaryReader reader;
+        private BinaryWriter writer;
+
+        private void Awake()
+        {
+            NetworkTransport.Init();
+        }
+
+        // Use this for initialization
+        private void Start()
+        {
+            var config = new ConnectionConfig();
+            state = config.AddChannel(QosType.Unreliable);
+            reliable = config.AddChannel(QosType.ReliableSequenced);
+            var topology = new HostTopology(config, 1);
+
+            host = NetworkTransport.AddHost(topology);
+
+            ms = new MemoryStream(data);
+            reader = new BinaryReader(ms);
+            writer = new BinaryWriter(ms);
+
+            NetworkTransport.StartBroadcastDiscovery(host, 9112, 1, 1, 0, broadcast, 1, 2000, out error);
+            PhoneServer.TestError(error);
+
+            Input.gyro.enabled = true;
+            Input.compass.enabled = true;
+        }
+
+        private float timer = 0;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            int rConn;
+            int rChan;
+            int rSize;
+
+            var rec = NetworkTransport.ReceiveFromHost(host, out rConn, out rChan, data, data.Length, out rSize,
+                out error);
+            PhoneServer.TestError(error);
+            ms.Position = 0;
+
+            switch (rec)
+            {
+                case NetworkEventType.DataEvent:
+                    if (rChan == reliable)
+                    {
+                        vibrate = reader.ReadSingle();
+                    }
+                    break;
+                case NetworkEventType.ConnectEvent:
+                    connection = rConn;
+                    isConnected = true;
+                    NetworkTransport.StopBroadcastDiscovery();
+                    break;
+                case NetworkEventType.DisconnectEvent:
+                    isConnected = false;
+                    NetworkTransport.StartBroadcastDiscovery(host, 9112, 1, 1, 0, broadcast, 1, 10, out error);
+                    break;
+                case NetworkEventType.Nothing:
+                    break;
+                case NetworkEventType.BroadcastEvent:
+                    break;
+            }
+
+            if (vibrate > 0.5f)
+            {
+                Handheld.Vibrate();
+            }
+
+            if (Time.time > timer && isConnected)
+            {
+                timer = Time.time + 0.0333f;
+                ms.Position = 0;
+                WriteStatus();
+                NetworkTransport.Send(host, connection, state, data, (int) ms.Position, out error);
+                PhoneServer.TestError(error);
+            }
+
+            text.text = "#" + number + " " + (isConnected ? "connected" : "searching") + "\nv" + vibrate;
+        }
+
+        public void SetNumber(int n)
+        {
+            number = n;
+        }
+
+        public void DoVibrate(float value)
+        {
+            Handheld.Vibrate();
+        }
+
+        public void WriteStatus()
+        {
+            writer.Write(number);
+            WriteQuaternion(Input.gyro.attitude);
+            WriteVector(Input.gyro.gravity);
+            WriteVector(Input.gyro.rotationRate);
+            WriteVector(Input.gyro.rotationRateUnbiased);
+            WriteVector(Input.gyro.userAcceleration);
+            WriteVector(Input.acceleration);
+            WriteVector(Input.compass.rawVector);
+            writer.Write(Input.compass.magneticHeading);
+            writer.Write(Input.compass.trueHeading);
+        }
+
+        private void WriteVector(Vector3 vec)
+        {
+            writer.Write(vec.x);
+            writer.Write(vec.y);
+            writer.Write(vec.z);
+        }
+
+        private void WriteQuaternion(Quaternion qua)
+        {
+            writer.Write(qua.x);
+            writer.Write(qua.y);
+            writer.Write(qua.z);
+            writer.Write(qua.w);
+        }
+    }
+}
---
+++ Assets/GyroPhone/PhoneServer.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+namespace VildNinja.GyroPhone
+{
+    public class PhoneServer : MonoBehaviour
+    {
+
+        public struct Data
+        {
+            public Quaternion gyroAttitude;
+            public Vector3 gyroGravity;
+            public Vector3 gyroRotationRate;
+            public Vector3 gyroRotationRateUnbiased;
+            public Vector3 gyroUserAcceleration;
+            public Vector3 acceleration;
+            public Vector3 compassRawVector;
+            public float compassMagneticHeading;
+            public float compassTrueHeading;
+        }
+
+        public readonly Data[] phones = new Data[10];
+        private readonly int[] clients = new int[10];
+
+        private int host;
+        private int state;
+        private int reliable;
+
+        private byte error;
+
+        private bool isConnected;
+
+        public Text debugText = null;
+
+        public readonly byte[] broadcast = new byte[1];
+        public readonly byte[] data = new byte[1000];
+
+        private MemoryStream ms;
+        private BinaryReader reader;
+        private BinaryWriter writer;
+
+        public static void TestError(byte error)
+        {
+            if (error > 0)
+            {
+                Debug.Log("Error " + error);
+            }
+        }
+
+        private void Awake()
+        {
+            NetworkTransport.Init();
+        }
+
+        // Use this for initialization
+        private void Start()
+        {
+
+            var config = new ConnectionConfig();
+            state = config.AddChannel(QosType.Unreliable);
+            reliable = config.AddChannel(QosType.ReliableSequenced);
+            var topology = new HostTopology(config, 10);
+
+            host = NetworkTransport.AddHost(topology, 9112);
+            NetworkTransport.SetBroadcastCredentials(host, 1, 1, 0, out error);
+
+            TestError(error);
+
+            ms = new MemoryStream(data);
+            reader = new BinaryReader(ms);
+            writer = new BinaryWriter(ms);
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            int rConn;
+            int rChan;
+            int rSize;
+
+            var rec = NetworkTransport.ReceiveFromHost(host, out rConn, out rChan, data, data.Length, out rSize,
+                out error);
+            TestError(error);
+            ms.Position = 0;
+
+            switch (rec)
+            {
+                case NetworkEventType.DataEvent:
+                    if (rChan == state)
+                    {
+                        int num = reader.ReadInt32();
+                        if (num >= 0 && num < 10)
+                        {
+                            clients[num] = rConn;
+                            Read(out phones[num].gyroAttitude);
+                            Read(out phones[num].gyroGravity);
+                            Read(out phones[num].gyroRotationRate);
+                            Read(out phones[num].gyroRotationRateUnbiased);
+                            Read(out phones[num].gyroUserAcceleration);
+                            Read(out phones[num].acceleration);
+                            Read(out phones[num].compassRawVector);
+                            phones[num].compassMagneticHeading = reader.ReadSingle();
+                            phones[num].compassTrueHeading = reader.ReadSingle();
+                        }
+                    }
+                    break;
+                case NetworkEventType.ConnectEvent:
+                    break;
+                case NetworkEventType.DisconnectEvent:
+                    break;
+                case NetworkEventType.Nothing:
+                    break;
+                case NetworkEventType.BroadcastEvent:
+
+                    int port;
+                    var bi = NetworkTransport.GetBroadcastConnectionInfo(host, out port, out error);
+                    TestError(error);
+                    NetworkTransport.Connect(host, bi, port, 0, out error);
+                    TestError(error);
+                    Debug.Log("Broadcast " + bi);
+
+                    break;
+            }
+
+            if (debugText != null)
+            {
+                string txt = "";
+                for (int i = 1; i <= 2; i++)
+                {
+                    txt += i + "\n";
+                    txt += "acc " + phones[i].acceleration + "\n";
+                    txt += "comMH " + phones[i].compassMagneticHeading + "\n";
+                    txt += "comTH " + phones[i].compassTrueHeading + "\n";
+                    txt += "comRaw " + phones[i].compassRawVector + "\n";
+                    txt += "gyroAtt " + phones[i].gyroAttitude + "\n";
+                    txt += "gyroRot " + phones[i].gyroRotationRate + "\n";
+                    txt += "gyroGrav " + phones[i].gyroGravity + "\n";
+                }
+                debugText.text = txt;
+            }
+        }
+
+        public void VibrateOne()
+        {
+            SetVibrate(1, 1);
+        }
+
+        public void StopVibrateOne()
+        {
+            SetVibrate(1, 0);
+        }
+
+        public void SetVibrate(int phone, float value)
+        {
+            ms.Position = 0;
+            writer.Write(value);
+            NetworkTransport.Send(host, clients[phone], reliable, data, 4, out error);
+            TestError(error);
+        }
+
+        private void Read(out Vector3 vec)
+        {
+            vec.x = reader.ReadSingle();
+            vec.y = reader.ReadSingle();
+            vec.z = reader.ReadSingle();
+        }
+
+        private void Read(out Quaternion qua)
+        {
+            qua.x = reader.ReadSingle();
+            qua.y = reader.ReadSingle();
+            qua.z = reader.ReadSingle();
+            qua.w = reader.ReadSingle();
+        }
+    }
+}
